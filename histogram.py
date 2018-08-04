d8fc7b2c97ffa90296d975213ec9c0bb2ef24761
ufgtb24 <1026773987@qq.com>
2018-08-04 13:33:13 +0800

    finish histogram, begin relation

---
+++ histogram.py
@@ -0,0 +1,163 @@
+import tensorflow as tf
+import numpy as np
+def kernel_mat(index):
+    '''
+    :param index: [num_batch,line_num,step_num,3] 需要被求kernel的点坐标
+    :return: [num_batch,line_num,step_num,27,3]
+    box中index处需要采样的所有kernel点的坐标，周围9个点（包括自己）
+    '''
+    x = [-1,0,1]
+    y = [-1,0,1]
+    z = [-1,0,1]
+    np.meshgrid()
+    x_t, y_t ,z_t= tf.meshgrid(x, y,z)
+    # flatten
+    x_t_flat = tf.reshape(x_t, [-1])  # [27]
+    y_t_flat = tf.reshape(y_t, [-1])
+    z_t_flat = tf.reshape(z_t, [-1])
+    
+    kernel = tf.stack([x_t_flat, y_t_flat, z_t_flat],axis=-1)  # [27,3]
+    index=tf.expand_dims(index, axis=-2) #[num_batch,line_num,step_num,1,3]
+    index=index+kernel #[num_batch,line_num,step_num,27,3]
+    
+
+    return index
+
+
+class Histogrammer:
+    def __init__(self, box, center_pos, tooth_ids):
+        #[batch_num,28,w,h,d]
+        # if a tooth is absence, its value is zero padded, tooth_num is fixed of 24
+        self.box=box
+        self.center_pos=center_pos
+        # [60 ] 0.2mm
+        self.center_list=np.linspace(-19,39,2)
+        
+        
+    def distance_mulpt(self, tooth_pair):
+        '''
+        get the vector of distance between specified two teeth
+        :param tooth_pair:
+        :return:[num_batch,plain_pt_num]
+        '''
+        #[batch,w,h,d]
+        box1=self.box[:,tooth_pair[0]]
+        box2=self.box[:,tooth_pair[1]]
+        #[batch,3]
+        pos1= self.center_pos[:, tooth_pair[0]]
+        pos2= self.center_pos[:, tooth_pair[1]]
+        # [num_batch,plain_pt_num]
+        pts_2teeth=self.dis_between_box(box1, pos1, box2, pos2)
+        return pts_2teeth
+    
+    def histogram(self, tooth_pair):
+        '''
+        get histogram of distance between two teeth count on given range
+        :param tooth_pair:
+        :param center_list: a list [nc]
+        :return: [num_batch,nc]
+        '''
+        internal= self.center_list[1] - self.center_list[0]
+        # [num_batch,plain_pt_num,nc]
+        d=tf.expand_dims(self.distance_mulpt(tooth_pair), axis=-1)
+        # [nc]
+        c=tf.constant(self.center_list)
+        # [num_batch,nc]
+        h=tf.reduce_sum(tf.minimum(tf.abs(d-c)/internal,0),axis=1)
+        return h
+    
+        
+    
+    def dis_between_box(self, box1, pos1, box2, pos2):
+        '''
+         get distance between corresponding points on two plains vertical to the direction from
+         one tooth to another
+
+        :param box1: [batch,w,h,d]
+        :param pos1: [batch,3]
+        :param box2: [batch,w,h,d]
+        :param pos2: [batch,3]
+        :return dis_pts: [num_batch,plain_pt_num]
+        '''
+        distance=tf.sqrt(tf.reduce_sum(tf.squared_difference(pos1,pos2),axis=1,keepdims=True))  #[batch,1]
+        # direction =tf.norm(pos2-pos1,axis=1)
+        direction=(pos2-pos1)/distance
+        batch_num=tf.shape(box1)[0]
+        z_vec_0=tf.zeros([batch_num,2])
+        z_vec_1=tf.ones([batch_num,1])
+        z_vec=tf.concat([z_vec_0,z_vec_1],axis=1) # [batch,3]
+        plain_vec1=tf.norm(tf.cross(direction, z_vec), axis=1) # [batch,3]
+        plain_vec2=tf.norm(tf.cross(direction, plain_vec1), axis=1) # [batch,3]
+        box1_c2e=self.dis_c2e(box1,direction,plain_vec1,plain_vec2) #[num_batch,plain_pt_num]
+        box2_c2e=self.dis_c2e(box2,-direction,plain_vec1,plain_vec2) #[num_batch,plain_pt_num]
+        dis_pts=tf.expand_dims(distance,axis=-1)-box1_c2e-box2_c2e
+        return dis_pts
+
+    def sample(self,box, indices):
+        '''
+        calculate the kernel value of a sample line
+        :param box: [batch_num,w,h,d]
+        :param indices: [num_batch,line_num,step_num,3]
+        :return: [num_batch,line_num,step_num]
+        '''
+        num_batch, line_num, step_num, _ = tf.shape(indices)
+        batch_idx = tf.range(0, num_batch)
+        batch_idx = tf.reshape(batch_idx, (num_batch, 1, 1, 1, 1))
+    
+        # [num_batch,line_num,step_num,27,3]
+        kernel_indices = kernel_mat(indices)
+    
+        b = tf.tile(batch_idx, (1, line_num, step_num, 27, 1))
+        # [num_batch,line_num,step_num,27,4]
+        kernel_indices = tf.concat([b, kernel_indices], -1)
+        # [num_batch,line_num,step_num,27]
+        kernel_sample = tf.gather_nd(box, kernel_indices)
+        # [num_batch,line_num,step_num]
+        kernel_value = tf.reduce_sum(kernel_sample, axis=-1)
+        # [num_batch,line_num,step_num]
+        center_sample = tf.gather_nd(box, indices)
+        # sum(X(i)+X(i+k),k)   [num_batch,line_num,step_num]
+        value = 27 * center_sample - kernel_value
+        return value
+
+    def dis_c2e(self, box, step_direction, plain_vec1, plain_vec2):
+        '''
+        distance from center to edge
+        :param step_direction: [batch,3]
+        :param plain_vec1: [batch,3]
+        :param plain_vec2: [batch,3]
+        :return:edge_dis [num_batch,plain_pt_num]
+        '''
+        batch_num=tf.shape(box)[0]
+        x=tf.linspace(-3,3,1)
+        y=tf.linspace(-3,3,1)
+        # [7,7]
+        x_t, y_t = tf.meshgrid(x, y)
+        x_t_flat = tf.reshape(x_t, [-1])  # [plain_pt_num]
+        y_t_flat = tf.reshape(y_t, [-1])
+        xy_flat=tf.expand_dims(tf.stack([x_t_flat,y_t_flat],axis=-1)) #[1,plain_pt_num,2]
+        xy_flat=tf.tile(xy_flat,tf.stack([batch_num, 1, 1])) #[batch,plain_pt_num,2]
+        vec_flat=tf.stack([plain_vec1,plain_vec2],axis=1) #[batch,2,3]
+        center=tf.constant([64,64,64],dtype=tf.float32)
+        pts_on_plain=tf.matmul(xy_flat,vec_flat)+center #[batch,plain_pt_num,3]
+        pts_on_plain=tf.expand_dims(pts_on_plain,axis=2) #[batch,plain_pt_num, 1, 3]
+        # the range to detect edge distance
+        step=tf.range(10,64) #[step_num]
+        step_direction=tf.expand_dims(step_direction, axis=1)  #[batch, 1, 3]
+        # [batch,plain_pt_num, step_num, 3]
+        pts_on_line=pts_on_plain+tf.expand_dims(step_direction * tf.expand_dims(step,axis=-1), axis=1)
+        pts_on_line=tf.to_int32(pts_on_line)
+        kernel_value=self.sample(box,pts_on_line) #[num_batch,plain_pt_num,step_num]
+        kernel_mul_dis=kernel_value*step #[num_batch,plain_pt_num,step_num]
+        edge_dis=tf.reduce_sum(tf.minimum(kernel_mul_dis,1),axis=-1) #[num_batch,plain_pt_num]
+        return edge_dis
+        
+        
+        
+        
+
+
+        
+        
+        
+        
--- relation.py
+++ relation.py
@@ -1,74 +1,212 @@
 import tensorflow as tf
-import numpy as np
-def kernel_mat(index):
+from histogram import Histogrammer as Hist
+class RelationNet(object):
     '''
-    :param index: [num_batch,line_num,line_pt_num,3] 需要被求kernel的点坐标
-    :return: [num_batch,line_num,line_pt_num,27,3]
-    box中index处需要采样的所有kernel点的坐标，周围9个点（包括自己）
+    需要建立四种 g_function，UL,UR,DL,DR，暂时不需要更细分
     '''
-    x = [-1,0,1]
-    y = [-1,0,1]
-    z = [-1,0,1]
-    np.meshgrid()
-    x_t, y_t ,z_t= tf.meshgrid(x, y,z)
-    # flatten
-    x_t_flat = tf.reshape(x_t, [-1])  # [27]
-    y_t_flat = tf.reshape(y_t, [-1])
-    z_t_flat = tf.reshape(z_t, [-1])
-    
-    kernel = tf.stack([x_t_flat, y_t_flat, z_t_flat],axis=-1)  # [27,3]
-    index=tf.expand_dims(index, axis=-2) #[num_batch,line_num,line_pt_num,1,3]
-    index=index+kernel #[num_batch,line_num,line_pt_num,27,3]
-    
-
-    return index
-
-def sample(box,indices):
-    '''
-    
-    :param box: [batch_num,l,l,l]
-    :param indices: [num_batch,line_num,line_pt_num,3]
-    :return: [num_batch,line_num,point_num]
-    '''
-    num_batch, line_num, line_pt_num, _ = tf.shape(indices)
-    batch_idx = tf.range(0, num_batch)
-    batch_idx = tf.reshape(batch_idx, (num_batch, 1, 1,1,1))
-    
-    # [num_batch,line_num,line_pt_num,27,3]
-    kernel_indices=kernel_mat(indices)
-    
-    b = tf.tile(batch_idx, (1, line_num, line_pt_num, 27,1))
-    # [num_batch,line_num,line_pt_num,27,4]
-    kernel_indices = tf.concat([b, kernel_indices], -1)
-    # [num_batch,line_num,line_pt_num,27]
-    kernel_sample=tf.gather_nd(box,kernel_indices)
-    # [num_batch,line_num,line_pt_num]
-    kernel_value=tf.reduce_sum(kernel_sample,axis=-1)
-    # [num_batch,line_num,line_pt_num]
-    center_sample=tf.gather_nd(box,indices)
-    # sum(X(i)+X(i+k),k)   [num_batch,line_num,line_pt_num]
-    value=27*center_sample-kernel_value
-    
-    return value
-
-class relation_net:
-    def __init__(self,box,tooth_ids):
-        #[batch_num,tooth_num,w,h,d]
-        # if a tooth is absence, its value is
-        self.box=box
-        # if a tooth is absence, its id is 0
-        self.ids=tooth_ids
+    def __init__(self,box):
+        '''
         
+        :param box_up: [batch_size,up_size,w,h,d]
+        :param box_low:  [batch_size,low_size,w,h,d]
+        '''
+        self.hist=Hist
+        self.box=box
+        self.phase=phase
+        self.upper_feature=upper_feature#[b,num/2,fc_size+4]
+        self.lowwer_feature=lowwer_feature
+        
+    def get_box(self,id):
+        if id<=15:
+            return self.box[:,]
+        
+    def build_pairs(self):
+        '''
+        2  - 15, 31 - 18
+        :return: a list of tuple
+        '''
+        up_id = list(range(2, 16))
+        low_id = list(range(31, 17, -1))
+        r1 = list(zip(up_id, low_id))
+        r2 = list(zip(up_id[1:7], low_id[:6]))
+        r3 = list(zip(up_id[-7:-1], low_id[-6:]))
+        r4 = list(zip(up_id[1:], up_id[:-1]))
+        r5 = list(zip(low_id[1:], low_id[:-1]))
+        self.r = r1 + r2 + r3 + r4 + r5
     
-def relation_loss(tooth_pair):
-    t
+    def histogram_loss(self):
+        self.
+        
+        
 
-def dis_c2e(box,direction,sample_length):
-    '''
-    distance from center to edge
-    :param direction: [batch,3]
-    :return:
-    '''
-    # indices[num_batch,line_num,line_pt_num,3]
-    step=
-    sample
+    def triangle_net(self, long, short, scope):
+        '''
+        :param long: [b, half_arch_size, obj_size]
+        :param short: [b, half_arch_size-1, obj_size]
+        :return:
+        '''
+        with tf.variable_scope(scope):
+            with tf.variable_scope('build_triangle'):
+                # part.shape = [batch_size, triangle_num,3*obj_size]
+                triangles=tf.concat([long[:,:-1,:],short,long[:,1:,:]],axis=2)
+
+                # triangles    [batch_size* triangle_num, 3*obj_size]
+                triangle_flat=tf.reshape(triangles,shape=[-1,triangles.shape[2].value])
+
+            # triangles    [batch_size* triangle_num, g_size]
+            triangle_flat=self.g_net(triangle_flat)
+            return triangle_flat
+
+    def g_net(self, triangle_flat):
+        with tf.variable_scope('g_net'):
+            units = self.params.g_units
+            fc = commen.dense_relu_batch(triangle_flat, output_size=units, phase=self.phase, scope='fc_1')
+            fc = commen.dense_relu_batch(fc, output_size=units, phase=self.phase, scope='fc_2')
+            fc = commen.dense_relu_batch(fc, output_size=units, phase=self.phase, scope='fc_3')
+            return fc #[b,g_size]
+
+    def build_relation(self):
+        '''
+        输入方向必须是
+        3  - 14
+        30 - 19
+        :param box: conv fc 之后的单个物体集合 [b,24,conv_fc]
+        :param pos: 物体的box下角位置集合
+        :return: [Tensor(b,g_output_size)] x11
+        '''
+        triangle_num=int(self.upper_feature.shape[1].value/2)
+        part_size=triangle_num+1
+
+        with tf.variable_scope('devide_parts'):
+            UL=self.upper_feature[:,:part_size,:]
+            UR=self.upper_feature[:,-part_size:,:]
+            DL=self.lowwer_feature[:,:part_size,:]
+            DR=self.lowwer_feature[:,-part_size:,:]
+
+        with tf.variable_scope('triangle_feature'):
+            # g_part    [batch_size * triangle_num, g_output_size]
+            triangles_1 =self.triangle_net(UL, DL[:, :-1, :], scope='triangle_1') # 3-30-4 to 8-25-9
+            triangles_2 =self.triangle_net(DL, UL[:, 1:, :], scope='triangle_2') # 30-4-29 to 25-9-24
+            triangles_3 =self.triangle_net(UR, DR[:, 1:, :], scope='triangle_3') # 8-24-9 to 13-19-14
+            triangles_4 =self.triangle_net(DR, UR[:, :-1, :], scope='triangle_4')# 25-8-24 to 20-13-19
+
+        with tf.variable_scope('extract_from_triangle'):
+            #left,mid,right are all of shape [batch_size, triangle_num,extract_size]
+            left_1,mid_1,right_1=self.extract_3_tooth(triangles_1,triangle_num,scope='triangle_1')
+            left_2,mid_2,right_2=self.extract_3_tooth(triangles_2,triangle_num,scope='triangle_2')
+            left_3,mid_3,right_3=self.extract_3_tooth(triangles_3,triangle_num,scope='triangle_3')
+            left_4,mid_4,right_4=self.extract_3_tooth(triangles_4,triangle_num,scope='triangle_4')
+
+        with tf.variable_scope('combineed_feature_to_tooth'):
+            with tf.variable_scope('part_ul'):
+                # shape = [batch_size, triangle_num-1,3*extract_size]
+                # 没有考虑后牙，所以是triangle_num -1
+                # 将从三组关系中提取出的关于某颗牙的特征组合起来
+                UL_relation_feature=tf.concat([right_1[:,:-1],mid_2[:,:-1],left_1[:,1:]],axis=2)
+                # ul_combined shape=[batch_size, tooth_num/4-1,combined_units]
+                ul_combined=self.combine_feature(UL_relation_feature,tooth_num=triangle_num -1)
+
+            with tf.variable_scope('part_dl'):
+                DL_relation_feature=tf.concat([right_2[:,:-1],mid_1[:,1:],left_2[:,1:]],axis=2)
+                dl_combined=self.combine_feature(DL_relation_feature,tooth_num=triangle_num -1)
+
+            with tf.variable_scope('part_ur'):
+                UR_relation_feature=tf.concat([right_3[:,:-1],mid_4[:,1:],left_3[:,1:]],axis=2)
+                ur_combined=self.combine_feature(UR_relation_feature,tooth_num=triangle_num -1)
+
+            with tf.variable_scope('part_dr'):
+                DR_relation_feature=tf.concat([right_4[:,:-1],mid_3[:,:-1],left_4[:,1:]],axis=2)
+                dr_combined=self.combine_feature(DR_relation_feature,tooth_num=triangle_num -1)
+
+        with tf.variable_scope('rns_featrue_for_arch'):
+            # 除了2颗后牙之外的上牙序列 [batch_size, tooth_num/2-2,combined_units]
+            # 可以用来和上牙 lstm 组合
+            relation_upper=tf.concat([ul_combined,ur_combined],axis=1)
+            relation_lowwer=tf.concat([dl_combined,dr_combined],axis=1)
+            return [relation_upper,relation_lowwer]
+
+
+    def combine_feature(self,relation_feature,tooth_num):
+        '''
+        将组合的关于单牙的feature 经过fc
+        :param relation_feature:
+        :param tooth_num:
+        :param scope:
+        :return:
+        '''
+        units=relation_feature.shape[2].value
+        relation_feature=tf.reshape(relation_feature,shape=[-1,units])
+        with tf.variable_scope('combine_net'):
+            units = self.params.combined_units
+            fc = commen.dense_relu_batch(relation_feature, output_size=units, phase=self.phase, scope='fc_1')
+            fc = commen.dense_relu_batch(fc, output_size=units, phase=self.phase, scope='fc_2')
+            fc = commen.dense_relu_batch(fc, output_size=units, phase=self.phase, scope='fc_3')
+            # [batch_size , triangle_num, extract_size]
+            fc=tf.reshape(fc,shape=[-1,tooth_num,units])
+            return fc
+
+
+
+    def extract_fc(self,g_outputs,triangle_num,scope):
+        with tf.variable_scope(scope):
+            units = self.params.extract_units
+            fc = commen.dense_relu_batch(g_outputs, output_size=units, phase=self.phase, scope='fc_1')
+            fc = commen.dense_relu_batch(fc, output_size=units, phase=self.phase, scope='fc_2')
+            fc = commen.dense_relu_batch(fc, output_size=units, phase=self.phase, scope='fc_3')
+            # [batch_size , triangle_num, extract_size]
+            fc=tf.reshape(fc,shape=[-1,triangle_num,units])
+            return fc
+
+    def extract_3_tooth(self,g_outputs,triangle_num,scope):
+        '''
+        从concat的关系组分别提取单个牙，并返回三个牙的fc的list
+        :param g_outputs: [batch_size * triangle_num , g_output_size]
+        :param scope: 上一层的范围
+        :return: a list for 3 indivifual teeth fc
+                of shape [batch_size , triangle_num , extracted_size]
+        '''
+        tooth_fc=[]
+        with tf.variable_scope(scope):
+            tooth_fc.append(self.extract_fc(g_outputs,triangle_num,scope='for_tooth1'))
+            tooth_fc.append(self.extract_fc(g_outputs,triangle_num,scope='for_tooth2'))
+            tooth_fc.append(self.extract_fc(g_outputs,triangle_num,scope='for_tooth3'))
+            return  tooth_fc
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
